4f4747f60342156813ac5838875015cd117778c4
Guilherme-alexandr <guilhermedafranca002@gmail.com>
2025-08-14 21:15:59 -0300

    Update importadores_route.py

--- app/routes/importadores_route.py
+++ app/routes/importadores_route.py
@@ -15,8 +15,8 @@ def importar_exemplos():
         return jsonify({"mensagem": "Os dados já foram importados, não é possível rodar novamente."}), 403
 
     base_dir = os.path.dirname(os.path.abspath(__file__))
-    clientes_path = os.path.join(base_dir, "../importadores/clientes_exemplo.docx")
-    contratos_path = os.path.join(base_dir, "../importadores/contratos_exemplo.docx")
+    clientes_path = os.path.join(base_dir, "importadores/clientes_exemplo.docx")
+    contratos_path = os.path.join(base_dir, "importadores/contratos_exemplo.docx")
 
     importar_clientes_docx(clientes_path, current_app)
     importar_contratos_docx(contratos_path, current_app)
