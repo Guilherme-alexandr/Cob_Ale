32db8ea83833ef89a29185dfe8ff8ffb917c2347
Guilherme-alexandr <guilhermedafranca002@gmail.com>
2025-08-14 21:05:26 -0300

    .

--- importadores/importar_clientes.py
+++ importadores/importar_clientes.py
@@ -2,44 +2,44 @@ import os
 from docx import Document
 from app.database import db
 from app.models.cliente import Cliente
-from run import create_app
 
-def importar_clientes_docx(caminho_arquivo):
-    doc = Document(caminho_arquivo)
-    clientes_importados = 0
+def importar_clientes_docx(caminho_arquivo, app):
+    with app.app_context():
+        doc = Document(caminho_arquivo)
+        clientes_importados = 0
 
-    for tabela in doc.tables:
-        for i, linha in enumerate(tabela.rows):
-            if i == 0:
-                continue
+        for tabela in doc.tables:
+            for i, linha in enumerate(tabela.rows):
+                if i == 0:
+                    continue
 
-            nome = linha.cells[0].text.strip()
-            cpf = linha.cells[1].text.strip()
-            numero = linha.cells[2].text.strip()
-            email = linha.cells[3].text.strip()
+                nome = linha.cells[0].text.strip()
+                cpf = linha.cells[1].text.strip()
+                numero = linha.cells[2].text.strip()
+                email = linha.cells[3].text.strip()
 
-            if not nome or not cpf:
-                print(f"Registro inválido na linha {i + 1}")
-                continue
+                if not nome or not cpf:
+                    print(f"Registro inválido na linha {i + 1}")
+                    continue
 
-            cliente = Cliente(
-                nome=nome,
-                cpf=cpf,
-                numero=numero,
-                email=email
-            )
-            db.session.add(cliente)
-            clientes_importados += 1
+                cliente = Cliente(
+                    nome=nome,
+                    cpf=cpf,
+                    numero=numero,
+                    email=email
+                )
+                db.session.add(cliente)
+                clientes_importados += 1
 
-    db.session.commit()
-    print(f"Importação concluída: {clientes_importados} clientes adicionados.")
-
+        db.session.commit()
+        print(f"Importação concluída: {clientes_importados} clientes adicionados.")
 
 if __name__ == "__main__":
+    from run import create_app
     app = create_app()
-    with app.app_context():
-        base_dir = os.path.dirname(os.path.abspath(__file__))
-        caminho = os.path.join(base_dir, "clientes_exemplo.docx")
-        importar_clientes_docx(caminho)
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    caminho = os.path.join(base_dir, "clientes_exemplo.docx")
+    importar_clientes_docx(caminho, app)
+
 
 # python -m importadores.importar_clientes
